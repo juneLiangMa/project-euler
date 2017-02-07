307834e4c2d1ac63519127a199933e6d6e99cc34
June Ma <June Ma>
2017-02-06 20:39:02 -0800

    problem1 add test suite to java project

--- src/tests/EulerProblemTests.java
+++ src/tests/EulerProblemTests.java
@@ -1,8 +1,22 @@
 package tests;
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.*;
 import org.junit.Test;
 
+/**
+ * Euler problem unit tests for validating and debugging
+ * correctness of the problem solutions.
+ * 
+ * The goal should be to verify the correctness of a problem
+ * solution entirely in unit tests, so that I only submit 
+ * an answer to the project euler site *once*.
+ * @author junel
+ * @version 1.0
+ * @since 2017/02/06
+ */
 public class EulerProblemTests {
-
+	
+	@Test
+	public void ProblemOneShouldInitialize() {
+		IEulerProblem p1 = new ProblemOne();
+	}
 }
