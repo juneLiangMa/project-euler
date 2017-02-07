3dfb6b2616055e44438b67c7bcb85fe319976275
June Ma <June Ma>
2017-02-06 19:47:08 -0800

    problem1 created interface and initialized the problem 1 class

---
+++ src/IEulerProblem.java
@@ -0,0 +1,11 @@
+/**
+ * The IEulerProblem interface defines the basic
+ * skeleton for all euler problem solutions.
+ * 
+ * @author junel
+ * @version 1.0
+ * @since 2017/02/06
+ */
+public interface IEulerProblem {
+	int Solve();
+}
--- src/ProblemOne.java
+++ src/ProblemOne.java
@@ -1,7 +1,32 @@
+import java.util.ArrayList;
 
-public class ProblemOne {
+public class ProblemOne implements IEulerProblem {
+	private int _limit;
+	private ArrayList<Integer> _factorsList;
+	
+	/// Problem description:
+	///Find the sum of all the multiples of 3 or 5 below 1000.
 	public ProblemOne() {
+		_factorsList = new ArrayList<Integer>();
+	}
+	
+	public ProblemOne(int limit, int[] factors) {
+		this();
+		_limit = limit;
+		for (int f : factors) {
+			_factorsList.add(f);
+		}
+	}
+	
+	public int Solve() {
+		if(_factorsList.isEmpty()) {
+			throw new IllegalStateException("No factors provided to solve the problem with.");
+		}
 		
+		int result = -1;
+		
+		
+		return result;
 	}
 	
 	
